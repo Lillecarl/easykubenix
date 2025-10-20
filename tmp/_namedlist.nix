03c69a0f63831bbe38f2dd55600a1383a8f86946
Carl Andersson <git@lillecarl.com>
2025-10-20 08:52:23 +0200

    Implement converting to and from _namedlist
    With this we'll be able to convert imported resources into attrsets that
    we can easily override with Nix. Then we convert back into lists that
    Kubernetes expects.

--- tmp/_namedlist.nix
+++ tmp/_namedlist.nix
@@ -1,31 +1,90 @@
 let
   pkgs = import <nixpkgs> { };
-  # Corrected function
-  transformNamedLists =
-    { lib }:
-    attrs:
-    (lib.mapAttrsRecursiveCond (as: !(as ? "_namedlist")) (
-      path: value:
-      if value._namedlist or false == true then
-        lib.pipe value [
-          (lib.filterAttrs (n: _: n != "_namedlist"))
-          lib.attrsToList
-          (lib.map (v: v.value // { inherit (v) name; }))
-        ]
-      else
-        value
-    ) attrs);
+  lib = pkgs.lib;
 
-  # Input with nested _namedlist
+  # Helper function to recursively walk a data structure
+  walk =
+    f: v:
+    if lib.isAttrs v then
+      f (lib.mapAttrs (n: walk f) v)
+    else if lib.isList v then
+      f (map (walk f) v)
+    else
+      f v;
+
+  walkExcludeAttrNames =
+    attrNames: f: v:
+    if lib.isAttrs v then
+      # Apply transform to the attrset itself, then map over its children
+      f (
+        lib.mapAttrs (
+          name: val:
+          # If the key is initContainers, just recurse without the transform
+          if lib.elem name attrNames then
+            val
+          # Otherwise, recurse normally
+          else
+            walkExcludeAttrNames attrNames f val
+        ) v
+      )
+    else if lib.isList v then
+      f (map (walkExcludeAttrNames attrNames f) v)
+    else
+      f v;
+
+  listToNamedList =
+    value:
+    if lib.isList value && (lib.all (lib.hasAttr "name") value) then
+      lib.pipe value [
+        (lib.map (x: {
+          inherit (x) name;
+          value = lib.removeAttrs x [ "name" ];
+        }))
+        lib.listToAttrs
+        (x: x // { _namedlist = true; })
+      ]
+    else
+      value;
+
+  namedListToList =
+    value:
+    if value._namedlist or false == true then
+      lib.mapAttrsToList (
+        name: val:
+        if !lib.isAttrs val then
+          throw "namedListToList error: Value for key '${name}' is not an attribute set."
+        else
+          val // { inherit name; }
+      ) (lib.removeAttrs value [ "_namedlist" ])
+    else
+      value;
+
   input = {
     level1 = {
-      systems = {
+      initContainers = {
         _namedlist = true;
-        x86_64-linux = {
-          host = "server-a";
+        acontainer = {
+          image = "bogus";
+          env = {
+            _namedlist = true;
+            VAR.value = "VALUE";
+          };
         };
-        aarch64-linux = {
-          host = "server-b";
+        bcontainer = {
+          image = "bogus";
+        };
+      };
+      containers = {
+        _namedlist = true;
+        acontainer = {
+          image = "bogus";
+          env = {
+            _namedlist = true;
+            VAR.value = "VALUE";
+          };
+        };
+        bcontainer = {
+          image = "bogus";
         };
       };
       other = {
@@ -34,4 +93,10 @@ let
     };
   };
 in
-transformNamedLists { inherit (pkgs) lib; } input
+rec {
+  ainputVersion = input;
+  blistVersion = (walk namedListToList) ainputVersion;
+  cattrVersion = (walkExcludeAttrNames [ "initContainers" ] listToNamedList) blistVersion;
+  dlistVersion = (walk namedListToList) cattrVersion;
+  eattrVersion = (walkExcludeAttrNames [ "initContainers" ] listToNamedList) dlistVersion;
+}
